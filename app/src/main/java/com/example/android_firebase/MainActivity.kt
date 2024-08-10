5b22ca6dccede253f3a10d8303f9f3740815b676
alvaradoCarolina <132953183+alvaradoCarolina@users.noreply.github.com>
2024-08-09 23:31:39 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/android_firebase/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.android_firebase
+
+
+import android.os.Bundle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_firebase.ui.theme.Android_firebaseTheme
+import com.google.firebase.FirebaseApp
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Código para guardar datos en Firestore
+        val db = FirebaseFirestore.getInstance()
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("TAG", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents.", exception)
+            }
+
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_firebaseTheme {
+        Greeting("Android")
+    }
+}
